adfddb9046ebb9b71181fdb861f8d1b96ef7b7c9
Pita's Life <74688531+PitaLife@users.noreply.github.com>
2020-11-19 07:50:14 +0700

    Add files via upload

---
+++ src/Turn.js
@@ -0,0 +1,9 @@
+class Turn {
+  constructor(currentRound, currentPlayer, currentSurvey) {
+    this.currentRound = currentRound;
+    this.currentPlayer = currentPlayer;
+    this.currentSurvey = currentSurvey;
+  }
+}
+
+export default Turn;
